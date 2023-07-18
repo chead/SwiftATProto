b9748adc4ae92565ca2d89e180c9a2bff686a17b
chead <chead@chead.net>
2023-07-17 18:25:03 -0700

    Initial Commit

---
+++ Tests/SwiftATProtoTests/SwiftATProtoTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftATProto
+
+final class SwiftATProtoTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftATProto().text, "Hello, World!")
+    }
+}
