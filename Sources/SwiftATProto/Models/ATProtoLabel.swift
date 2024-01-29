bfc8999c688eff4a9459ef3296725214b42e97ad
chead <chead@chead.net>
2024-01-28 18:49:14 -0800

    Fixed optional fields in ATProtoLabel.

--- Sources/SwiftATProto/Models/ATProtoLabel.swift
+++ Sources/SwiftATProto/Models/ATProtoLabel.swift
@@ -10,12 +10,12 @@ import Foundation
 public struct ATProtoLabel: Decodable {
     public let src: String
     public let uri: String
-    public let cid: String
+    public let cid: String?
     public let val: String
-    public let neg: Bool
+    public let neg: Bool?
     public let cts: String
 
-    public init(src: String, uri: String, cid: String, val: String, neg: Bool, cts: String) {
+    public init(src: String, uri: String, cid: String?, val: String, neg: Bool?, cts: String) {
         self.src = src
         self.uri = uri
         self.cid = cid
