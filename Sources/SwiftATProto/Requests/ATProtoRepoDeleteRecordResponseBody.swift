cf5042e1e263ff47b757c652b9c2f6cfdb02094b
chead <chead@chead.net>
2024-02-07 21:21:10 -0800

    Added delete record request response body (empty).

---
+++ Sources/SwiftATProto/Requests/ATProtoRepoDeleteRecordResponseBody.swift
@@ -0,0 +1,11 @@
+//
+//  ATProtoRepoDeleteRecordResponseBody.swift
+//  
+//
+//  Created by Christopher Head on 2/7/24.
+//
+
+import Foundation
+
+public struct ATProtoRepoDeleteRecordResponseBody: Decodable {
+}
