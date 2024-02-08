a364ad0b6dfa35a534a3d005d15e80a43081aa0a
chead <chead@chead.net>
2024-02-07 23:31:03 -0800

    Added empty response body.

--- Sources/SwiftATProto/Requests/ATProtoEmptyResponseBody.swift
+++ Sources/SwiftATProto/Requests/ATProtoEmptyResponseBody.swift
@@ -1,8 +1,12 @@
 //
-//  File.swift
-//  
+//  ATProtoEmptyResponseBody.swift
+//
 //
 //  Created by Christopher Head on 2/7/24.
 //
 
 import Foundation
+
+public struct ATProtoEmptyResponseBody: Decodable {
+
+}
