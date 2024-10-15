c8a6545342d4850918cda64b9e40b3bb65b50bdd
chead <chead@chead.net>
2024-10-15 16:19:53 -0700

    Fixed non-required params

--- Sources/SwiftATProto/ATProtoHTTPRequest.swift
+++ Sources/SwiftATProto/ATProtoHTTPRequest.swift
@@ -19,56 +19,56 @@ public struct ATProtoHTTPRequest {
                 if !parameters.keys.contains([parameter]) {
                     throw ATProtoHTTPRequestError.requiredParameters
                 }
+            }
+        }
 
-                if let properties = requestable.parameters?.properties {
-                    for property in properties {
-                        switch(property.value) {
-                        case .boolean(_) where parameters[property.key] is Bool:
-                            if let booleanParameter = parameters[property.key] as? Bool {
-                                url.append(queryItems: [URLQueryItem(name: property.key, value: String(booleanParameter))])
-                            } else {
-                                throw ATProtoHTTPRequestError.invalidParameter
-                            }
-
-                        case .integer(_):
-                            if let integerParameter = parameters[property.key] as? Int {
-                                url.append(queryItems: [URLQueryItem(name: property.key, value: String(integerParameter))])
-                            } else {
-                                throw ATProtoHTTPRequestError.invalidParameter
-                            }
-
-                        case .string(_), .unknown(_):
-                            if let stringParameter = parameters[property.key] as? String {
-                                url.append(queryItems: [URLQueryItem(name: property.key, value: stringParameter)])
-                            } else {
-                                throw ATProtoHTTPRequestError.invalidParameter
-                            }
-
-                        case .array(_):
-                            if let arrayParameters = parameters[property.key] as? [Any] {
-                                for arrayParameter in arrayParameters {
-                                    switch arrayParameter {
-                                    case let boolean as Bool:
-                                        url.append(queryItems: [URLQueryItem(name: property.key, value: String(boolean))])
-
-                                    case let integer as Int:
-                                        url.append(queryItems: [URLQueryItem(name: property.key, value: String(integer))])
-
-                                    case let string as String:
-                                        url.append(queryItems: [URLQueryItem(name: property.key, value: string)])
-
-                                    default:
-                                        throw ATProtoHTTPRequestError.invalidParameter
-                                    }
-                                }
-                            } else {
-                                throw ATProtoHTTPRequestError.invalidParameter
-                            }
-
-                        default:
-                            throw ATProtoHTTPRequestError.invalidParameter
-                        }
+        if let properties = requestable.parameters?.properties {
+            for property in properties {
+                switch(property.value) {
+                case .boolean(_) where parameters[property.key] is Bool:
+                    if let booleanParameter = parameters[property.key] as? Bool {
+                        url.append(queryItems: [URLQueryItem(name: property.key, value: String(booleanParameter))])
+                    } else {
+                        throw ATProtoHTTPRequestError.invalidParameter
                     }
+
+                case .integer(_):
+                    if let integerParameter = parameters[property.key] as? Int {
+                        url.append(queryItems: [URLQueryItem(name: property.key, value: String(integerParameter))])
+                    } else {
+                        throw ATProtoHTTPRequestError.invalidParameter
+                    }
+
+                case .string(_), .unknown(_):
+                    if let stringParameter = parameters[property.key] as? String {
+                        url.append(queryItems: [URLQueryItem(name: property.key, value: stringParameter)])
+                    } else {
+                        throw ATProtoHTTPRequestError.invalidParameter
+                    }
+
+                case .array(_):
+                    if let arrayParameters = parameters[property.key] as? [Any] {
+                        for arrayParameter in arrayParameters {
+                            switch arrayParameter {
+                            case let boolean as Bool:
+                                url.append(queryItems: [URLQueryItem(name: property.key, value: String(boolean))])
+
+                            case let integer as Int:
+                                url.append(queryItems: [URLQueryItem(name: property.key, value: String(integer))])
+
+                            case let string as String:
+                                url.append(queryItems: [URLQueryItem(name: property.key, value: string)])
+
+                            default:
+                                throw ATProtoHTTPRequestError.invalidParameter
+                            }
+                        }
+                    } else {
+                        throw ATProtoHTTPRequestError.invalidParameter
+                    }
+
+                default:
+                    throw ATProtoHTTPRequestError.invalidParameter
                 }
             }
         }
