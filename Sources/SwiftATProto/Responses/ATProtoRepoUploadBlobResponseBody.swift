a10cde53f2289ab8786becc6e91a37b75470c990
chead <chead@chead.net>
2024-11-02 16:34:00 -0700

    Added upload blob response body

---
+++ Sources/SwiftATProto/Responses/ATProtoRepoUploadBlobResponseBody.swift
@@ -0,0 +1,10 @@
+//
+//  ATProtoRepoUploadBlobResponseBody.swift
+//  SwiftATProto
+//
+//  Created by Christopher Head on 11/2/24.
+//
+
+public struct ATProtoRepoUploadBlobResponseBody: Decodable {
+    public let blob: ATProtoBlob
+}
