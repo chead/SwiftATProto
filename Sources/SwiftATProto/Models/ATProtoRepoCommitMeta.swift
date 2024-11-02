1b7586fdebd28b6ff5454656e17aa8c758089042
chead <chead@chead.net>
2024-11-01 19:55:14 -0700

    Added commit meta

---
+++ Sources/SwiftATProto/Models/ATProtoRepoCommitMeta.swift
@@ -0,0 +1,11 @@
+//
+//  ATProtoRepoCommitMeta.swift
+//  SwiftATProto
+//
+//  Created by Christopher Head on 11/1/24.
+//
+
+public struct ATProtoRepoCommitMeta: Decodable {
+    public let cid: String
+    public let rev: String
+}
